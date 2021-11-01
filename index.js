1144118a632440ffdfbc2cdd3297c58dc444a07f
seenso <>
2021-11-01 11:05:47 -0800

    commit tree 1a181dd2df2e6a36fae9961a1322ae46d5738e45

--- index.js
+++ index.js
@@ -6,11 +6,23 @@ function distanceFromHqInBlocks(street) {
 function distanceFromHqInFeet(street) {
   const blockLength = 264;
   return Math.abs((street - 42) * blockLength);
-}
+};
 
 distanceTravelledInFeet = (start, end) => {
   const blockLength = 264;
   return Math.abs((end - start) * blockLength);
-}
+};
 
-function calculatesFarePrice() {}
+function calculatesFarePrice(start, destination) {
+  const distance = distanceTravelledInFeet(start, destination);
+
+  if (distance <= 400) {
+    return 0;
+  } else if (distance > 400 && distance <= 2000) {
+    return 0.02 * (distance - 400);
+  } else if (distance > 2000 && distance <= 2500) {
+    return 25.00 * (distance - 2000);
+  } else {
+    return 'cannot travel that far';
+  }
+};
