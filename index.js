8dd317901b8776f0490535b73d10fe494fcb40a9
seenso <>
2021-11-01 10:25:30 -0800

    passed tests for distanceFromHqInBlocks() & distanceFromHqInFeet()

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(street) {
+  return Math.abs(street - 42);
+};
+
+function distanceFromHqInFeet(street) {
+  const blockLength = 264;
+  return Math.abs((street - 42) * blockLength);
+}
