d41247bc1aa9924a4378229d032db2d1e8c6118b
seenso <>
2021-11-01 11:09:35 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -6,11 +6,23 @@ function distanceFromHqInBlocks(street) {
 function distanceFromHqInFeet(street) {
   const blockLength = 264;
   return Math.abs((street - 42) * blockLength);
-}
+};
 
 distanceTravelledInFeet = (start, end) => {
   const blockLength = 264;
   return Math.abs((end - start) * blockLength);
-}
+};
 
-function calculatesFarePrice() {}
+function calculatesFarePrice(start, destination) {
+  const distance = distanceTravelledInFeet(start, destination);
+
+  if (distance <= 400) {
+    return 0;
+  } else if (400 < distance && distance <= 2000) {
+    return 0.02 * (distance - 400);
+  } else if (2000 < distance && distance <= 2500) {
+    return 25; //charges a flat $25
+  } else {
+    return 'cannot travel that far';
+  }
+};
